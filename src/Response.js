a632ff11fd18d923752d8bfe5fd021bfbab741a4
Aaron Cox <aaron@greymass.com>
2020-05-19 18:22:05 -0400

    cleanup

--- src/Response.js
+++ src/Response.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import { Header, List, Segment } from 'semantic-ui-react';
-import ReactJson from 'react-json-view';
-import { CodeBlock, monokai } from 'react-code-blocks'
+import { List, Segment } from 'semantic-ui-react';
+import { Serialize } from 'eosjs'
 
-import { JsonRpc, Api, Serialize } from 'eosjs'
 const transactionAbi = require('eosjs/src/transaction.abi.json')
 const transactionTypes: Map<string, Serialize.Type> = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), transactionAbi)
 
@@ -62,7 +60,6 @@ class Transaction extends Component {
       },
     ]
     return tests.map((testCase) => {
-      console.log(testCase, testCase.test(response), response)
       return {
         ...testCase,
         res: testCase.test(response),
@@ -78,9 +75,7 @@ class Transaction extends Component {
     return type.deserialize(buffer)
   }
   render() {
-    const { response } = this.props;
     const tests = this.testResponse()
-    console.log(tests)
     return (
       <React.Fragment>
         <Segment style={{ overflowX: 'scroll' }}>
